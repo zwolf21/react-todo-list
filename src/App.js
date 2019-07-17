2a2a6b149697df4d0a8bec39e30c74db7f8f4a87
HS Moon <pbr112@naver.com>
2019-07-18 05:06:59 +0900

    페이지 템플릿 컴포넌트생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 
+import PageTemplate from "./components/PageTemplate";
+
 export class App extends Component {
   render() {
-    return <div>리액트</div>;
+    return <PageTemplate>리액트</PageTemplate>;
   }
 }
 

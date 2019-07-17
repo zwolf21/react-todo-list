3452333bf4b43949fdb912227c6cd80dec62ec2c
HS Moon <pbr112@naver.com>
2019-07-18 06:19:43 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import PageTemplate from "./components/PageTemplate";
-import TodoInput from "./components/TodoInput";
 import TodoInputContainer from "./containers/TodoInputContainer";
 
 export class App extends Component {
